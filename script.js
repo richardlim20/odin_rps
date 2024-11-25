92954872112e3a023d74b6937e6e6a03cfa3b0e9
Richard Lim <s3901013@student.rmit.edu.au>
2024-11-25 18:33:43 +1100

    refactor: create ui

--- script.js
+++ script.js
@@ -20,22 +20,22 @@ const getComputerChoice = () => {
     return computerChoice
 }
 
-const getHumanChoice = () => {
-    let humanInput = prompt("Rock paper or scissors?")
-    humanInput = humanInput.toLocaleLowerCase()
-    let humanChoice = "";
-    switch(humanInput){
-        case "rock":
-            humanChoice = Choices.Rock
-            break;
-        case "paper":
-            humanChoice = Choices.Paper;
-            break;
-        case "scissors":
-            humanChoice = Choices.Scissors;
-    }
-    return humanChoice
-}
+// const getHumanChoice = () => {
+//     let humanInput = prompt("Rock paper or scissors?")
+//     humanInput = humanInput.toLocaleLowerCase()
+//     let humanChoice = "";
+//     switch(humanInput){
+//         case "rock":
+//             humanChoice = Choices.Rock
+//             break;
+//         case "paper":
+//             humanChoice = Choices.Paper;
+//             break;
+//         case "scissors":
+//             humanChoice = Choices.Scissors;
+//     }
+//     return humanChoice
+// }
 
 // let humanScore = 0;
 // let computerScore = 0;
@@ -48,37 +48,70 @@ const playGame = () => {
     let humanScore = 0;
     let computerScore = 0;
     let roundNumber = 1;
-    let humanChoice = "";
-    let computerChoice = "";
+    let finalResults = "";
 
     const playRound = (humanChoice, computerChoice) => {
-        console.log("You choose " + humanChoice + " Computer Chooses " + computerChoice)
+        choicesUI.innerHTML = "You choose " + humanChoice + " Computer Chooses " + computerChoice
         if (humanChoice === Choices.Rock && computerChoice === Choices.Scissors ||
             humanChoice === Choices.Scissors && computerChoice === Choices.Paper ||
             humanChoice === Choices.Paper && computerChoice === Choices.Rock)
             {
-                console.log("You Win!")
+                roundResultsUI.innerHTML = "You Win!";
                 humanScore++
             }
         else if (humanChoice === Choices.Rock && computerChoice === Choices.Paper ||
                 humanChoice === Choices.Scissors && computerChoice === Choices.Rock ||
                 humanChoice === Choices.Paper && computerChoice === Choices.Scissors)
             {
-                console.log("You Lose!")
+                roundResultsUI.innerHTML = "You Lose!";
                 computerScore++
             }
     
         else if(humanChoice === computerChoice){
-            console.log("its a draw!");
+            roundResultsUI.innerHTML = "its a draw!";
+        }
+    }
+    const handleButtonClick = (choiceChosen) => {
+        let gameOver = false;
+        if (humanScore === 5 || computerScore === 5){
+            gameOver = true;
+        }
+        if(!gameOver){
+            humanChoice = choiceChosen;
+            computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
+            roundNumber++
+            updateUI()
+        }
+        else{
+            finalResults = "The final results are: " + humanScore + " to " + computerScore;
+            updateUI()
         }
     }
 
-    for (roundNumber; roundNumber <= 5; roundNumber++){
-        console.log("Round: " + roundNumber)
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice)
+    const updateUI = () => {
+        roundNumberUI.innerHTML = roundNumber;
+        humanScoreUI.innerHTML = humanScore;
+        computerScoreUI.innerHTML = computerScore;
+        finalResultsUI.innerHTML = finalResults;
     }
+
+    rockButton.addEventListener("click", () => handleButtonClick(Choices.Rock));
+    scissorsButton.addEventListener("click", () => handleButtonClick(Choices.Scissors));
+    paperButton.addEventListener("click", () => handleButtonClick(Choices.Paper));
+    const roundNumberUI = document.getElementById("round-number");
+    roundNumberUI.innerHTML = roundNumber;
+    const humanScoreUI = document.getElementById("human-score");
+    humanScoreUI.innerHTML = humanScore;
+    const computerScoreUI = document.getElementById("computer-score");
+    computerScoreUI.innerHTML = computerScore;
+    const finalResultsUI = document.getElementById("final-results");
+    const choicesUI = document.getElementById("round-choices")
+    const roundResultsUI = document.getElementById("round-results")
 }
+const rockButton = document.getElementById("rock-btn");
+const scissorsButton = document.getElementById("scissors-btn");
+const paperButton = document.getElementById("paper-btn");
+
 
 playGame()
